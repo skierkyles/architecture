d1d0d6eb10ec31f98ecdc67834959f32190ce649
Kyle Swanson <skierkyles@gmail.com>
2015-01-26 19:21:30 -0700

    Lab 1 start

---
+++ Lab_1/asm.s
@@ -0,0 +1,15 @@
+  PUBLIC __iar_program_start
+
+  SECTION .intvec:CODE (2)
+  DATA
+  DC32 0x20008000U
+  DC32 __iar_program_start
+
+  SECTION LAB1:CODE (2)
+  THUMB
+  
+__iar_program_start
+  B main
+main NOP
+  B main
+  END
